ae4d0a3f7a4145eb21494df9642977d40fff3a17
Adam <ajwieme@gmail.com>
2017-11-22 09:13:57 -0600

    /data

--- scripts/get_g2p.sh
+++ scripts/get_g2p.sh
@@ -5,5 +5,5 @@ MODEL="$2"
 LANG="$3"
 SETTING="$4"
 
-python "$ROOT"/phone-based-reinflection/scripts/conll2onmt.py "$ROOT"/phone-based-reinflection/data/"$LANG"-train-"$SETTING" "$ROOT"/phone-based-reinflection/onmt-character-inputs
-python "$ROOT"/OpenNMT-py/translate.py -model "$MODEL" -src "$ROOT"/phone-based-reinflection/onmt-character-inputs/"$LANG"-train-"$SETTING"-src.txt -tgt "$ROOT"/phone-based-reinflection/onmt-character-inputs/"$LANG"-train-"$SETTING"-tgt.txt  -output "$ROOT"/phone-based-reinflection/onmt-phone-inputs/"$LANG"-"$SETTING" -replace_unk
+python "$ROOT"/phone-based-reinflection/scripts/conll2onmt.py "$ROOT"/phone-based-reinflection/data/"$LANG"-train-"$SETTING" "$ROOT"/phone-based-reinflection/data/onmt-character-inputs
+python "$ROOT"/OpenNMT-py/translate.py -model "$MODEL" -src "$ROOT"/phone-based-reinflection/data/onmt-character-inputs/"$LANG"-train-"$SETTING"-src.txt -tgt "$ROOT"/phone-based-reinflection/data/onmt-character-inputs/"$LANG"-train-"$SETTING"-tgt.txt  -output "$ROOT"/phone-based-reinflection/data/onmt-phone-inputs/"$LANG"-"$SETTING" -replace_unk
