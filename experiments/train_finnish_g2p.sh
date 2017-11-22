a63ad85073df811875ef6deec0f4508ffcc1c66f
Adam <ajwieme@gmail.com>
2017-11-22 08:05:42 -0600

    update shell script to reflect dirname change

--- experiments/train_finnish_g2p.sh
+++ experiments/train_finnish_g2p.sh
@@ -6,6 +6,6 @@ LANG="finnish"
 LANG_CODE="fin"
 EPOCHS="20"
 
-python "$ROOT"/phone-based-feature-experiments/scripts/phoible2onmt.py -i "$ROOT"/phone-based-feature-experiments/data/pron_data/gold_data_train -o "$ROOT"/phone-based-feature-experiments/data/opennmt-formatted-pron_data -l $LANG -lc $LANG_CODE
-python "$ROOT"/OpenNMT-py/preprocess.py -train_src  "$ROOT"/phone-based-feature-experiments/data/opennmt-formatted-pron_data/$LANG-train-src -train_tgt  "$ROOT"/phone-based-feature-experiments/data/opennmt-formatted-pron_data/$LANG-train-tgt -valid_src  "$ROOT"/phone-based-feature-experiments/data/opennmt-formatted-pron_data/$LANG-dev-src -valid_tgt  "$ROOT"/phone-based-feature-experiments/data/opennmt-formatted-pron_data/$LANG-dev-tgt -save_data "$ROOT"/phone-based-feature-experiments/g2p-models/$LANG
-python "$ROOT"/OpenNMT-py/train.py -data "$ROOT"/phone-based-feature-experiments/g2p-models/$LANG -save_model "$ROOT"/phone-based-feature-experiments/g2p-models/$LANG -epochs "$EPOCHS"
+python "$ROOT"/phone-based-reinflection/scripts/phoible2onmt.py -i "$ROOT"/phone-based-reinflection/data/pron_data/gold_data_train -o "$ROOT"/phone-based-reinflection/data/opennmt-formatted-pron_data -l $LANG -lc $LANG_CODE
+python "$ROOT"/OpenNMT-py/preprocess.py -train_src  "$ROOT"/phone-based-reinflection/data/opennmt-formatted-pron_data/$LANG-train-src -train_tgt  "$ROOT"/phone-based-reinflection/data/opennmt-formatted-pron_data/$LANG-train-tgt -valid_src  "$ROOT"/phone-based-reinflection/data/opennmt-formatted-pron_data/$LANG-dev-src -valid_tgt  "$ROOT"/phone-based-reinflection/data/opennmt-formatted-pron_data/$LANG-dev-tgt -save_data "$ROOT"/phone-based-reinflection/g2p-models/$LANG
+python "$ROOT"/OpenNMT-py/train.py -data "$ROOT"/phone-based-reinflection/g2p-models/$LANG -save_model "$ROOT"/phone-based-reinflection/g2p-models/$LANG -epochs "$EPOCHS"
