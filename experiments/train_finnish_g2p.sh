40e415c446b409b3703002c557195460e5ccc4a6
Adam <ajwieme@gmail.com>
2017-11-26 17:09:30 -0600

    smaller hidden layer and embedding size

--- experiments/train_finnish_g2p.sh
+++ experiments/train_finnish_g2p.sh
@@ -8,4 +8,4 @@ EPOCHS="50"
 
 python "$ROOT"/phone-based-reinflection/scripts/phoible2onmt.py -i "$ROOT"/phone-based-reinflection/data/pron_data/gold_data_train -o "$ROOT"/phone-based-reinflection/data/opennmt-formatted-pron_data -l $LANG -lc $LANG_CODE
 python "$ROOT"/OpenNMT-py/preprocess.py -train_src  "$ROOT"/phone-based-reinflection/data/opennmt-formatted-pron_data/$LANG-train-src -train_tgt  "$ROOT"/phone-based-reinflection/data/opennmt-formatted-pron_data/$LANG-train-tgt -valid_src  "$ROOT"/phone-based-reinflection/data/opennmt-formatted-pron_data/$LANG-dev-src -valid_tgt  "$ROOT"/phone-based-reinflection/data/opennmt-formatted-pron_data/$LANG-dev-tgt -save_data "$ROOT"/phone-based-reinflection/g2p-models/$LANG
-python "$ROOT"/OpenNMT-py/train.py -data "$ROOT"/phone-based-reinflection/g2p-models/$LANG -save_model "$ROOT"/phone-based-reinflection/g2p-models/$LANG -epochs "$EPOCHS" -brnn  -word_vec_size 300 -rnn_size 300 -gpuid 0
+python "$ROOT"/OpenNMT-py/train.py -data "$ROOT"/phone-based-reinflection/g2p-models/$LANG -save_model "$ROOT"/phone-based-reinflection/g2p-models/$LANG -epochs "$EPOCHS" -brnn  -word_vec_size 100 -rnn_size 100 -gpuid 0
