09fde78fab32d2a32d8e5769c227e04dfeac772e
Adam <ajwieme@gmail.com>
2017-11-26 16:18:07 -0600

    changed some train params

--- experiments/train_finnish_g2p.sh
+++ experiments/train_finnish_g2p.sh
@@ -4,8 +4,8 @@
 ROOT="$1"
 LANG="finnish"
 LANG_CODE="fin"
-EPOCHS="20"
+EPOCHS="50"
 
 python "$ROOT"/phone-based-reinflection/scripts/phoible2onmt.py -i "$ROOT"/phone-based-reinflection/data/pron_data/gold_data_train -o "$ROOT"/phone-based-reinflection/data/opennmt-formatted-pron_data -l $LANG -lc $LANG_CODE
 python "$ROOT"/OpenNMT-py/preprocess.py -train_src  "$ROOT"/phone-based-reinflection/data/opennmt-formatted-pron_data/$LANG-train-src -train_tgt  "$ROOT"/phone-based-reinflection/data/opennmt-formatted-pron_data/$LANG-train-tgt -valid_src  "$ROOT"/phone-based-reinflection/data/opennmt-formatted-pron_data/$LANG-dev-src -valid_tgt  "$ROOT"/phone-based-reinflection/data/opennmt-formatted-pron_data/$LANG-dev-tgt -save_data "$ROOT"/phone-based-reinflection/g2p-models/$LANG
-python "$ROOT"/OpenNMT-py/train.py -data "$ROOT"/phone-based-reinflection/g2p-models/$LANG -save_model "$ROOT"/phone-based-reinflection/g2p-models/$LANG -epochs "$EPOCHS" -gpuid 0
+python "$ROOT"/OpenNMT-py/train.py -data "$ROOT"/phone-based-reinflection/g2p-models/$LANG -save_model "$ROOT"/phone-based-reinflection/g2p-models/$LANG -epochs "$EPOCHS" -brnn -gpuid 0
