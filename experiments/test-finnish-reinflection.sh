74eaa262c0fa96bf5be19000fe6d2dc9403f8e94
Adam <ajwieme@gmail.com>
2017-11-29 11:41:24 -0700

    fixing calling one bash script from another

--- experiments/test-finnish-reinflection.sh
+++ experiments/test-finnish-reinflection.sh
@@ -5,7 +5,7 @@ LANG="finnish"
 
 for s in low medium high;
 do
-  sh "$ROOT"/phone-based-reinflection/scripts/get_g2p_test.sh $ROOT/phone-based-reinflection/g2p-models/finnish_acc_96.28_ppl_1.18_e20.pt $LANG $s
+  /bin/bash "$ROOT"/phone-based-reinflection/scripts/get_g2p_test.sh $ROOT $ROOT/phone-based-reinflection/g2p-models/finnish_acc_96.28_ppl_1.18_e20.pt $LANG $s
   python "$ROOT"/OpenNMT-py/translate.py -model "$MODEL" -src "$ROOT"/phone-based-reinflection/data/opennmt-formatted-pron_data/finnish-test-src -tgt "$ROOT"/phone-based-reinflection/data/opennmt-formatted-pron_data/finnish-test-tgt  -output "$ROOT"/phone-based-reinflection/data/opennmt-formatted-pron_data/"$MODEL_FN"-pred.txt -replace_unk
 python "$ROOT"/phone-based-reinflection/scripts/evalm.py --gold "$ROOT"/phone-based-reinflection/data/opennmt-formatted-pron_data/finnish-test-tgt --guess "$ROOT"/phone-based-reinflection/data/opennmt-formatted-pron_data/"$MODEL_FN"-pred.txt
 done;
--- experiments/train_finnish_reinflection.sh
+++ experiments/train_finnish_reinflection.sh
@@ -1,12 +1,13 @@
-#! /bin/sh
+#! /bin/bash
 #Preproceses, generates files, and trains on the data
+#G2P Model must be hardcoded, and is assumed to have already been trained
 
 ROOT="$1"
 LANG="finnish"
 
 for s in low medium high;
 do
-  sh "$ROOT"/phone-based-reinflection/scripts/get_g2p_train.sh $ROOT/phone-based-reinflection/g2p-models/finnish_acc_96.28_ppl_1.18_e20.pt $LANG $s
+  /bin/bash "$ROOT"/phone-based-reinflection/scripts/get_g2p_train.sh $ROOT $ROOT/phone-based-reinflection/g2p-models/finnish_acc_96.28_ppl_1.18_e20.pt $LANG $s
   python "$ROOT"/OpenNMT-py/preprocess.py -train_src  "$ROOT"/phone-based-reinflection/data/onmt-phone-inputs/"$LANG"-train-"$s"-src.txt -train_tgt  "$ROOT"/phone-based-reinflection/data/onmt-phone-inputs/"$LANG"-train-"$s"-tgt.txt -valid_src  "$ROOT"/phone-based-reinflection/onmt-phone-inputs/"$LANG"-dev-src.txt -valid_tgt  "$ROOT"/phone-based-reinflection/onmt-phone-inputs/"$LANG"-dev-tgt.txt -save_data "$ROOT"/phone-based-reinflection/reinflection-phoneme-models/"$LANG"-"$s"
   python "$ROOT"/OpenNMT-py/train.py -data "$ROOT"/phone-based-reinflection/reinflection-phoneme-models/"$LANG"-"$s" -save_model "$ROOT"/phone-based-reinflection/reinflection-phoneme-models/"$LANG"-"$s"-model -epochs 50 -brnn -gpuid 0
 done;
