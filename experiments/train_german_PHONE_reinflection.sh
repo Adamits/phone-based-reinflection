7ca6df1742276463351c8c3126c7355b041099dd
Adam <ajwieme@gmail.com>
2018-01-20 16:26:07 -0700

    correct output

--- experiments/train_german_PHONE_reinflection.sh
+++ experiments/train_german_PHONE_reinflection.sh
@@ -6,13 +6,13 @@ ROOT="$1"
 LANG="german"
 
 # Format the dev files, and get the g2p
-python "$ROOT"/phone-based-reinflection/scripts/conll2onmt-epitran_PHONES.py "$LANG" "$ROOT"/"$LANG"-dev
+python "$ROOT"/phone-based-reinflection/scripts/conll2onmt-epitran_PHONES.py "$LANG" "$ROOT"/"$LANG"-dev "$ROOT"/phone-based-reinflection/data/onmt-phone-inputs/"$LANG"-train-"$s"
 
 # Try all settings
 for s in low medium high;
 do
   # Format the train data, and get the g2p
-  python "$ROOT"/phone-based-reinflection/scripts/conll2onmt-epitran_PHONES.py "$LANG" "$ROOT"/"$LANG"-train-"$s"
+  python "$ROOT"/phone-based-reinflection/scripts/conll2onmt-epitran_PHONES.py "$LANG" "$ROOT"/"$LANG"-train-"$s" "$ROOT"/phone-based-reinflection/data/onmt-phone-inputs/"$LANG"-train-"$s"
   # Preprocess and train an ONMT model on files formatted above
   python "$ROOT"/OpenNMT-py/preprocess.py -train_src  "$ROOT"/phone-based-reinflection/data/onmt-phone-inputs/"$LANG"-train-"$s"-src.txt -train_tgt  "$ROOT"/phone-based-reinflection/data/onmt-phone-inputs/"$LANG"-train-"$s"-tgt.txt -valid_src  "$ROOT"/phone-based-reinflection/data/onmt-phone-inputs/"$LANG"-dev-src.txt -valid_tgt  "$ROOT"/phone-based-reinflection/data/onmt-phone-inputs/"$LANG"-dev-tgt.txt -save_data "$ROOT"/phone-based-reinflection/reinflection-phoneme-models/"$LANG"-"$s"
   python "$ROOT"/OpenNMT-py/train.py -data "$ROOT"/phone-based-reinflection/reinflection-phoneme-models/"$LANG"-"$s" -save_model "$ROOT"/phone-based-reinflection/reinflection-phoneme-models/"$LANG"-"$s"-model -epochs 50 -brnn -gpuid 0
