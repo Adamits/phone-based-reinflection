eafdbe58c84313cd3269c3196d69d3e76edeff8a
Adam <ajwieme@gmail.com>
2018-01-20 18:19:37 -0700

    remove settings from test, and misused arg

--- experiments/test_german_PHONE_reinflection.sh
+++ experiments/test_german_PHONE_reinflection.sh
@@ -8,9 +8,5 @@ MODEL_FN="$(basename $MODEL)"
 # Format the test data, and get the g2p
 python "$ROOT"/phone-based-reinflection/scripts/conll2onmt-epitran_PHONES.py "$LANG" "$ROOT"/phone-based-reinflection/data/"$LANG"-uncovered-test "$ROOT"/phone-based-reinflection/data/onmt-phone-inputs/"$LANG"-test
 
-# Try all settings
-for s in low medium high;
-do
-  python "$ROOT"/OpenNMT-py/translate.py -model "$MODEL" -src "$ROOT"/phone-based-reinflection/data/onmt-phone-inputs/"$LANG"-test-src.txt -tgt "$ROOT"/phone-based-reinflection/data/onmt-phone-inputs/"$LANG"-test-tgt.txt  -output "$ROOT"/phone-based-reinflection/preds/"$MODEL_FN"-pred.txt -replace_unk
-  python "$ROOT"/phone-based-reinflection/scripts/evalm.py --gold "$ROOT"/phone-based-reinflection/data/onmt-phone-inputs/"$LANG"-test-tgt.txt --guess "$ROOT"/phone-based-reinflection/preds/"$MODEL_FN"-pred.txt -gpuid 0
-done;
+python "$ROOT"/OpenNMT-py/translate.py -model "$MODEL" -src "$ROOT"/phone-based-reinflection/data/onmt-phone-inputs/"$LANG"-test-src.txt -tgt "$ROOT"/phone-based-reinflection/data/onmt-phone-inputs/"$LANG"-test-tgt.txt  -output "$ROOT"/phone-based-reinflection/preds/"$MODEL_FN"-pred.txt -replace_unk -gpuid 0
+python "$ROOT"/phone-based-reinflection/scripts/evalm.py --gold "$ROOT"/phone-based-reinflection/data/onmt-phone-inputs/"$LANG"-test-tgt.txt --guess "$ROOT"/phone-based-reinflection/preds/"$MODEL_FN"-pred.txt
