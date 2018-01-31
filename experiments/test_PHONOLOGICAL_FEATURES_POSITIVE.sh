d78f089d70aba62bf6ce44b77e21c0c2754e3de3
Adam <ajwieme@gmail.com>
2018-01-31 16:07:42 -0700

    namespacing

--- experiments/test_PHONOLOGICAL_FEATURES_POSITIVE.sh
+++ experiments/test_PHONOLOGICAL_FEATURES_POSITIVE.sh
@@ -6,7 +6,7 @@ LANG="$3"
 MODEL_FN="$(basename $MODEL)"
 
 # Format the test data, and get the g2p
-python "$ROOT"/phone-based-reinflection/scripts/conll2onmt_POSITIVE_PHONOLOGICAL_FEATURES.py "$LANG" "$ROOT"/phone-based-reinflection/data/"$LANG"-uncovered-test "$ROOT"/phone-based-reinflection/data/onmt-phone-feature-inputs/"$LANG"-test
+python "$ROOT"/phone-based-reinflection/scripts/conll2onmt_POSITIVE_PHONOLOGICAL_FEATURES.py "$LANG" "$ROOT"/phone-based-reinflection/data/"$LANG"-uncovered-test "$ROOT"/phone-based-reinflection/data/onmt-positive-phone-feature-inputs/"$LANG"-test
 
-python "$ROOT"/OpenNMT-py/translate.py -model "$MODEL" -src "$ROOT"/phone-based-reinflection/data/onmt-phone-feature-inputs/"$LANG"-test-src.txt -tgt "$ROOT"/phone-based-reinflection/data/onmt-phone-feature-inputs/"$LANG"-test-tgt.txt  -output "$ROOT"/phone-based-reinflection/preds/"$MODEL_FN"-pred.txt -replace_unk
-python "$ROOT"/phone-based-reinflection/scripts/evalm.py --gold "$ROOT"/phone-based-reinflection/data/onmt-phone-feature-inputs/"$LANG"-test-tgt.txt --guess "$ROOT"/phone-based-reinflection/preds/"$MODEL_FN"-pred.txt
+python "$ROOT"/OpenNMT-py/translate.py -model "$MODEL" -src "$ROOT"/phone-based-reinflection/data/onmt-positive-phone-feature-inputs/"$LANG"-test-src.txt -tgt "$ROOT"/phone-based-reinflection/data/onmt-positive-phone-feature-inputs/"$LANG"-test-tgt.txt  -output "$ROOT"/phone-based-reinflection/preds/"$MODEL_FN"-pred.txt -replace_unk
+python "$ROOT"/phone-based-reinflection/scripts/evalm.py --gold "$ROOT"/phone-based-reinflection/data/onmt-positive-phone-feature-inputs/"$LANG"-test-tgt.txt --guess "$ROOT"/phone-based-reinflection/preds/"$MODEL_FN"-pred.txt
